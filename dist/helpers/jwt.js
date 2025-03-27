2fad038e844ebbd84a77f7c07cb265ccd4190c3a
Moises <castdevel@gmail.com>
2025-03-26 21:02:52 -0400

    Fix(Token Expiration time decreased)

--- dist/helpers/jwt.js
+++ dist/helpers/jwt.js
@@ -9,7 +9,7 @@ const generarJWT = (id, name) => {
     return new Promise((resolve, reject) => {
         const payload = { id, name };
         jsonwebtoken_1.default.sign(payload, process.env.JWT_SECRET, {
-            expiresIn: '1200'
+            expiresIn: '20m'
         }, (err, token) => {
             if (err) {
                 console.log(err);
